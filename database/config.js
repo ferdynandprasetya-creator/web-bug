793ad5e133e49f568317e17e38f76a2ca284de21
ferdynandprasetya-creator <ferdynandprasetya@gmail.com>
2026-07-09 11:07:31 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+// ©AiiSigma
+
+module.exports = {
+  domain: "http://server.lynzzofficial.com", // Ganti sma domain panel lu dan pastiin harus *http*
+  port: "2070" // Ganti Portnoy di panel network.
+};
